49d3393504ab0e66b963e613fa136802077bdcee
Petr Mitin <petr09mitin@mail.ru>
2020-11-28 14:55:18 +0300

    Backend now passes all the fcc tests

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track', { useMongoClient: true } )
-
+mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track', { useUnifiedTopology: true, useNewUrlParser: true } )
 app.use(cors())
 
 app.use(bodyParser.urlencoded({extended: false}))
@@ -18,6 +17,91 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const userSchema = new mongoose.Schema({name: {type: String, required: true}});
+const User = mongoose.model('User', userSchema);
+
+const exerciseSchema = new mongoose.Schema({
+  userId: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User'},
+  description: {type: String, required: true},
+  duration: {type: Number, required: true},
+  date: {type: Date, default: new Date().toISOString()}
+});
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+const getDateYYYYMMDD = date => date.toISOString().split('T')[0]
+
+const checkIfUserExists = async id => {
+  const user = await User.findOne({_id: id})
+  return user || false;
+}
+
+const validateNewExercise = (req, res, next) => {
+  const body = req.body;
+  if (body.userId && body.description && body.duration) {
+    return next();
+  }
+  next({status: 400, message: 'bad request'});
+}
+
+app.post('/api/exercise/new-user', (req, res, next) => {
+  User.create({name: req.body.username}, (err, data) => {
+    if (err) return next(err);
+    else res.json({_id: data._id, username: data.name});
+  })
+})
+
+app.get('/api/exercise/users', (req, res, next) => {
+    User.find({}, (err, data) => {
+      if (err) return next(err);
+      res.send(data);
+    })
+})
+
+app.post('/api/exercise/add', validateNewExercise, async (req, res, next) => {
+    const body = req.body;
+    const user = await checkIfUserExists(body.userId);
+    if (!user) return next({status: 400, message: 'bad request'});
+    const newExercise = {
+      userId: body.userId,
+      description: body.description,
+      duration: body.duration
+    };
+    if (body.date) newExercise.date = new Date(body.date);
+    Exercise.create(newExercise, (err, data) => {
+      if (err) return next(err);
+      res.json({
+        _id: user._id,
+        username: user.name,
+        description: data.description,
+        duration: data.duration,
+        date: data.date.toDateString()
+      });
+    })
+})
+
+app.get('/api/exercise/log', async (req, res, next) => {
+  const query = req.query;
+  const user = await checkIfUserExists(query.userId);
+  if (!user) return next({status: 400, message: "bad request"});
+  let dateQuery = {};
+  const limit = query.limit ? parseInt(query.limit) : 0;
+  dateQuery.$gte = query.from ? new Date(query.from).toISOString() : 0;
+  if (query.to) dateQuery.$lte = new Date(query.to).toISOString();
+  const data = await Exercise.find({userId: user._id, date: dateQuery}).limit(limit);
+  const resObj = {
+    "_id": user._id,
+    "username": user.name,
+    "count": data.length,
+    "log": data.map(data => ({
+      _id: data._id,
+      userId: data.userId,
+      description: data.description,
+      duration: data.duration,
+      date: getDateYYYYMMDD(data.date)
+    }))
+  }
+  res.json(resObj);
+})
 
 // Not found middleware
 app.use((req, res, next) => {
